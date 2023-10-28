ac7f0fcaab483c15e1ae32a1b8187a992b5c8ab9
Hp Laptop <prem.vishal.bagul@gmail.com>
2023-10-28 23:58:07 +0530

    Completed

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+import styles from "./Contact.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Contact = () => {
+  return (
+    <footer id="contact" className={styles.container}>
+      <div className={styles.text}>
+        <h2>Contact</h2>
+        <p>Feel free to reach out!</p>
+      </div>
+      <ul className={styles.links}>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
+          <a href="mailto:prem.vishal.bagul@gmail.com">prem.vishal.bagul@gmail.com</a>
+        </li>
+        <li className={styles.link}>
+          <img
+            src={getImageUrl("contact/linkedinIcon.png")}
+            alt="LinkedIn icon"
+          />
+          <a href="https://www.linkedin.com/in/vishal-bagul-34186b218">linkedin.com/vishal-bagul</a>
+        </li>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
+          <a href="https://www.github.com/vishalbagul1">github.com/vishalbagul1</a>
+        </li>
+      </ul>
+    </footer>
+  );
+};
